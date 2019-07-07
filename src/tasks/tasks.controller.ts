a2d3a4cf8b34408774aca7134ab56f449cbf33e5
Andrija Kević <andrija@pointjupiter.com>
2019-07-07 18:19:38 +0200

    add patch task status function and route

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { PatchTaskDto } from './dto/patch-task.dto';
 
 @Controller ('tasks')
 export class TasksController {
@@ -25,5 +26,10 @@ export class TasksController {
     @Delete('/:id')
     deleteTask (@Param('id') id: string): Task[] {
       return this.tasksService.deleteTask(id)
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus (@Param('id') id: string, @Body() PatchTaskDto: PatchTaskDto) {
+      return this.tasksService.updateTaskStatus(id, PatchTaskDto)
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { Task, TaskStatus } from './task.model'
 import * as uuid from 'uuid/v1'
 import { CreateTaskDto } from './dto/create-task.dto';
+import { PatchTaskDto } from './dto/patch-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -30,8 +31,16 @@ export class TasksService {
   }
 
   deleteTask (id: string): Task[] {
-    const taskToDelete = this.tasks.find(task => task.id === id)
+    const taskToDelete = this.getTaskById(id)
     this.tasks.splice(this.tasks.indexOf(taskToDelete), 1)
     return this.tasks
+  }
+
+  updateTaskStatus (id: string, PatchTaskDto: PatchTaskDto): Task {
+    const { status } = PatchTaskDto
+    const taskToUpdate = this.getTaskById(id)
+    taskToUpdate.status = status
+
+    return taskToUpdate
   }
 }
