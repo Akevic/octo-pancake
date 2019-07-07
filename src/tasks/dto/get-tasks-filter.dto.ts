8a342523574044b5164b84e2250291fb45d5808e
Andrija Kević <andrija@pointjupiter.com>
2019-07-07 21:24:40 +0200

    implement swagger

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,10 @@
 import { TaskStatus } from '../task.model';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class GetTasksFilterDto {
+  @ApiModelProperty()
   status: TaskStatus
+
+  @ApiModelProperty()
   search: string
 }
--- src/tasks/dto/patch-task.dto.ts
+++ src/tasks/dto/patch-task.dto.ts
@@ -1,4 +1,6 @@
 import { TaskStatus } from '../task.model';
+import { ApiModelProperty } from '@nestjs/swagger';
 export class PatchTaskDto {
+  @ApiModelProperty()
   status: TaskStatus
 }
